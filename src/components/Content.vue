e78720320f6ca5e23ece5f056d4931d2fe7e27f1
Nevio Werlen <LordNevi@outlook.com>
2024-02-28 14:11:16 +0100

    update files

---
+++ src/components/Content.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="jumbotron">
+    <h1 class="display-4 text-center">Body</h1>
+  </div>
+</template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="jumbotron">
+      <h1 class="display-4 text-center">NavBar</h1>
+    </div>
+  </template>
+  
