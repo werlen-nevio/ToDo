546565f06d1fa89f13fc10380097ea3502fde733
Nevio Werlen <LordNevi@outlook.com>
2024-03-06 16:50:08 +0100

    Add edit modal

---
+++ src/components/EditModal.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">ToDo bearbeiten</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true"><i class="fa-solid fa-x"></i></span>
+                </button>
+            </div>
+            <form>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="Titel">Titel *</label>
+                        <input type="text" class="form-control" id="Titel" placeholder="Titel">
+                    </div>
+                    <div class="form-group">
+                        <label for="Beschreibung">Beschreibung *</label>
+                        <input type="text" class="form-control" id="Beschreibung" placeholder="Beschreibung">
+                    </div>
+                    <div class="form-group">
+                        <label for="Datum">Datum *</label>
+                        <input type="datetime-local" class="form-control" id="Datum">
+                    </div>
+                    <div class="form-group">
+                        <label for="Kategorie">Kategorie</label>
+                        <select class="form-control" name="Kategorie" id="Kategorie">
+                            <option value="0">---</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-primary">Speichern</button>
+                </div>
+            </form>
+            </div>
+        </div>
+    </div>
+</template>
--- src/components/ToDoComponent.vue
+++ src/components/ToDoComponent.vue
@@ -1,34 +1,51 @@
+<script setup>
+    import EditModal from './EditModal.vue';
+</script>
+
 <template>
     <div class="ToDoComponent col-md-12 clearfix">
-        <div class="ToDoCheckbox col-md-1 float-left">
-            <input class="ToDoCheckboxInput" type="checkbox" name="" id="">
-        </div>
-        <div class="ToDoComponentDetails col-md-11 float-left">
-            <h2 class="ToDoComponentDetail ToDoTitel"> {{ Titel }} </h2>
-            <p class="ToDoComponentDetail ToDoBeschreibung"> {{ Beschreibung }} </p>
-            <p class="ToDoComponentDetail ToDoDatum"> {{ Datum }} </p>
-        </div>
-        <div class="TodoIcon col-md-1 float-left">
-            <i class="fa-solid fa-pen"></i>
-        </div>
+      <div class="ToDoCheckbox col-md-1 float-left">
+        <input class="ToDoCheckboxInput" type="checkbox" name="" id="">
+      </div>
+      <div class="ToDoComponentDetails col-md-11 float-left">
+        <h2 class="ToDoComponentDetail ToDoTitel">{{ Titel }}</h2>
+        <p class="ToDoComponentDetail ToDoBeschreibung">{{ Beschreibung }}</p>
+        <p class="ToDoComponentDetail ToDoDatum">{{ Datum }}</p>
+      </div>
+      <div class="TodoIcon col-md-1 float-left">
+        <button @click="openModal" class="btn-edit">
+          <i class="fa-solid fa-pen"></i>
+        </button>
+      </div>
     </div>
-</template>
-
-<script>
-    export default {
-        props: {
-            Titel: {
-                type: String,
-                required: true
-            },
-            Beschreibung: {
-                type: String,
-                required: true
-            },
-            Datum: {
-                type: Date,
-                required: true
-            }
-        }
+  
+    <EditModal />
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      Titel: {
+        type: String,
+        required: true
+      },
+      Beschreibung: {
+        type: String,
+        required: true
+      },
+      Datum: {
+        type: Date,
+        required: true
+      },
+      ID: {
+        type: Number,
+        required: true
+      }
+    },
+    methods: {
+      openModal() {
+        $('#myModal').modal('show');
+      }
     }
-</script>
+  }
+  </script>  
