e15c50b5ad912ff76fff7161fe241332df5e5809
Nevio Werlen <LordNevi@outlook.com>
2024-03-06 14:16:54 +0100

    beautify navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,5 @@
 <template>
-    <div class="jumbotron">
-      <h1 class="display-4 text-center">NavBar</h1>
-    </div>
-  </template>
-  
+  <div class="jumbotron">
+    <h1 class="display-4 text-center">NavBar</h1>
+  </div>
+</template>
