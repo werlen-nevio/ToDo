8df0f5cd7799ab823b352eaa48158e23c9bb2cee
Nevio Werlen <LordNevi@outlook.com>
2024-03-06 15:39:39 +0100

    Fix navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <script setup>
-import NavBar from './components/NavBar.vue'
- //import Content from './components/Content.vue'
+  import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
   <NavBar />
-  <!--<Content /> -->
 </template>
