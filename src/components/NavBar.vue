18b2ad7a6e4448467a4021e130ea273eab02b390
Tobias Holzer <tobias.holzer@edu.vs.ch>
2024-03-06 14:10:10 +0100

    NavBar 1.1

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -9,18 +9,22 @@ export default {
 </script>
 
 <template>
-  <div class="mainStyle text-center">
-    <Box box-title="Heute" class="mainButtons" /><br>
-    <Box box-title="Heute" class="mainButtons" /><br>
-    <Box box-title="Geplant" class="mainButtons" /><br>
-  </div>
-  <div class="mainStyle">
-    <p>Kategorien</p>
-    <Box box-title="Kategorie 1" class="categoryButtons" /><br>
-    <Box box-title="+" class="categoryButtons" />
-
-  </div>
-  <div class="logoStyle">
-    <img src="../img/logo.png" alt="logo" id="logo">
+  <div id="container">
+    <!-- Main Buttons -->
+    <div class="text-center">
+      <Box box-title="Heute" class="mainButtons" /><br>
+      <Box box-title="Heute" class="mainButtons" /><br>
+      <Box box-title="Geplant" class="mainButtons" /><br>
+    </div>
+    <!-- Category Buttons -->
+    <div>
+      <p>Kategorien</p>
+      <Box box-title="Kategorie 1" class="categoryButtons" /><br>
+      <Box box-title="+" class="categoryButtons" /><br>
+    </div>
+    <!-- Logo -->
+    <div class="logoStyle">
+      <img src="../img/logo.png" alt="logo" id="logo">
+    </div>
   </div>
 </template>
