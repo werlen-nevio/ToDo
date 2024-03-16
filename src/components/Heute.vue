b111259ee5f2e33141766fc0418a9b1f422f2f9b
Nevio Werlen <LordNevi@outlook.com>
2024-03-16 17:30:06 +0100

    Fix Heute so it displays all todos of today

--- src/components/Heute.vue
+++ src/components/Heute.vue
@@ -1,7 +1,25 @@
-<script setup>
-    import ToDoComponent from './ToDoComponent.vue'
-</script>
-
 <template>
-    <ToDoComponent Titel="Titel" Beschreibung="Beschreibung" Datum="22.03.2024 10:10" :ID="Number(1)"/>
-</template>
+    <div>
+      <div v-for="(todo, index) in filteredTodos" :key="index">
+        <ToDoComponent :Titel="todo.Titel" :Beschreibung="todo.Beschreibung" :Datum="todo.Datum" :ID="todo.id"/>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import ToDoComponent from './ToDoComponent.vue';
+  
+  const todos = JSON.parse(localStorage.getItem('todos')) || [];
+  
+  const filteredTodos = todos.filter(todo => {
+    const today = new Date();
+    const todoDate = new Date(todo.Datum);
+  
+    return (
+      today.getFullYear() === todoDate.getFullYear() &&
+      today.getMonth() === todoDate.getMonth() &&
+      today.getDate() === todoDate.getDate()
+    );
+  });
+  </script>
+  
