2bf2ef0ee91bd07e74ea287d95eff87d8e0b646a
Nevio Werlen <LordNevi@outlook.com>
2024-03-20 13:07:46 +0100

    order by date

--- src/components/Alle.vue
+++ src/components/Alle.vue
@@ -4,7 +4,7 @@
 
 <template>
     <div>
-        <Datum v-for="(datum, index) in datumsArray" :key="index" :Datum="datum" />
+        <Datum v-for="(datum, index) in orderedDatumsArray" :key="index" :Datum="datum" />
     </div>
 </template>
 
@@ -18,8 +18,9 @@ todos.forEach(todo => {
     if (todo.Datum && todo.Datum.trim() !== '') {
         const date = new Date(todo.Datum);
         const formattedDate = `${date.getDate().toString().padStart(2, '0')}.${(date.getMonth() + 1).toString().padStart(2, '0')}.${date.getFullYear()}`;
-        
         datumsArray.push(formattedDate);
     }
 });
+
+const orderedDatumsArray = datumsArray.sort((a, b) => a.date - b.date);
 </script>
