aa32a64d6ccf885323807af38b03c44d701a007b
Nevio Werlen <LordNevi@outlook.com>
2024-03-20 13:42:36 +0100

    Add pinia

--- src/components/Alle.vue
+++ src/components/Alle.vue
@@ -1,5 +1,22 @@
 <script setup>
     import Datum from './Datum.vue'
+
+    const todos = JSON.parse(localStorage.getItem('todos'));
+    const datumsArray = [];
+
+    if (todos && Array.isArray(todos)) { // Überprüfe, ob todos nicht null ist und ein Array ist
+        todos.forEach(todo => {
+            if (todo.Datum && todo.Datum.trim() !== '') {
+                const date = new Date(todo.Datum);
+                const formattedDate = `${date.getDate().toString().padStart(2, '0')}.${(date.getMonth() + 1).toString().padStart(2, '0')}.${date.getFullYear()}`;
+                datumsArray.push(formattedDate);
+            }
+        });
+    } else {
+        console.error("No todos found in localStorage or todos is not an array");
+    }
+
+    const orderedDatumsArray = datumsArray.sort((a, b) => a.date - b.date);
 </script>
 
 <template>
@@ -7,20 +24,3 @@
         <Datum v-for="(datum, index) in orderedDatumsArray" :key="index" :Datum="datum" />
     </div>
 </template>
-
-
-<script>
-const todos = JSON.parse(localStorage.getItem('todos'));
-
-const datumsArray = [];
-
-todos.forEach(todo => {
-    if (todo.Datum && todo.Datum.trim() !== '') {
-        const date = new Date(todo.Datum);
-        const formattedDate = `${date.getDate().toString().padStart(2, '0')}.${(date.getMonth() + 1).toString().padStart(2, '0')}.${date.getFullYear()}`;
-        datumsArray.push(formattedDate);
-    }
-});
-
-const orderedDatumsArray = datumsArray.sort((a, b) => a.date - b.date);
-</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import './Assets/main.css'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+const app = createApp(App);
+const pinia = createPinia();
+app.use(pinia);
 
-createApp(App).mount('#app')
+app.mount('#app');
