2a69f82f1e948e016159c6112a8c9013ffc0f8ac
Nevio Werlen <LordNevi@outlook.com>
2024-03-20 14:08:39 +0100

    save todos to localstorage

--- src/Store/todoStore.js
+++ src/Store/todoStore.js
@@ -2,23 +2,29 @@ import { defineStore } from 'pinia';
 
 export const useTodosStore = defineStore('todos', {
   state: () => ({
-    todos: [],
+    todos: JSON.parse(localStorage.getItem('todos')) || [],
   }),
   actions: {
     addTodo(newTodo) {
       this.todos.push(newTodo);
+      this.saveTodos();
     },
     editTodo(updatedTodo) {
       const todoIndex = this.todos.findIndex(todo => todo.id === updatedTodo.id);
       if (todoIndex !== -1) {
         this.todos[todoIndex] = updatedTodo;
+        this.saveTodos();
       }
     },
     deleteTodo(id) {
       this.todos = this.todos.filter(todo => todo.id !== id);
+      this.saveTodos();
     },
     getTodos() {
       return this.todos;
+    },
+    saveTodos() {
+      localStorage.setItem('todos', JSON.stringify(this.todos));
     },
   },
 });
