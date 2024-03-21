1b4015e2a2faf1dc416b1359e0e53708aaccc55f
Nevio Werlen <LordNevi@outlook.com>
2024-03-21 11:27:22 +0100

    add Geplant

--- src/components/Geplant.vue
+++ src/components/Geplant.vue
@@ -1,9 +1,37 @@
 <script setup>
-    import ToDoComponent from './ToDoComponent.vue'
-    import Datum from './Datum.vue'
+import { computed } from 'Vue'
+import { useTodosStore } from '../Store/todoStore.js'
+import Datum from './Datum.vue'
+
+const todosStore = useTodosStore();
+
+const orderedFutureDatumsArray = computed(() => {
+  const now = new Date();
+  const seenDates = new Set();
+  return todosStore.todos
+    .filter(todo => {
+      if (!todo.Datum || todo.Datum.trim() === '') return false; // Ignore todos without Datum
+      const date = new Date(todo.Datum);
+      return date > now; // Only consider future dates
+    })
+    .map(todo => {
+      const date = new Date(todo.Datum);
+      const formattedDate = `${date.getFullYear().toString().padStart(4, '0')}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
+      if (!seenDates.has(formattedDate)) {
+        seenDates.add(formattedDate);
+        return { date: formattedDate, original: todo.Datum };
+      }
+      return null;
+    })
+    .filter(date => date !== null)
+    .sort((a, b) => new Date(a.date) - new Date(b.date))
+    .map(item => item.original);
+});
 </script>
 
+
 <template>
-    <Datum Datum="10.10.2023"/>
-    <ToDoComponent Titel="Titel" Beschreibung="Beschreibung" Datum="22.03.2024 10:10" :ID="Number(1)"/>
+  <div>
+    <Datum v-for="datum in orderedFutureDatumsArray" :key="datum" :Datum="datum" />
+  </div>
 </template>
